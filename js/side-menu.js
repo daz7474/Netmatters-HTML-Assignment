9831b0bbfa8467d88034d8d1a449f10c5cee6faf
Darren Lindsay <daz7474@gmail.com>
2024-03-26 14:10:46 +0000

    side menu slide out

---
+++ js/side-menu.js
@@ -0,0 +1,19 @@
+// ======================================
+// Side Menu
+// ======================================
+
+// Get ref to burger menu button
+const menuBtn = document.querySelector(".hamburger");
+const sideMenu = document.querySelector(".side-menu");
+const burgerBars = document.querySelectorAll(".bar");
+// Get ref to body element
+const body = document.querySelector("body");
+
+// On click, toggles class "side-menu-open" to show/hide
+menuBtn.addEventListener("click", () => {
+  sideMenu.classList.toggle("side-menu-open");
+  // Toggles margins on the body when menu is open
+  body.classList.toggle("side-menu-margins");
+  // Toggles the class "x" to add/remove a cross
+  burgerBars.forEach(bar => bar.classList.toggle('x'));
+});
