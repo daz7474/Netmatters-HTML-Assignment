fcaa28ba2e574c1282092933341ab47e52ad81ec
Darren Lindsay <daz7474@gmail.com>
2024-04-11 22:05:47 +0100

    Cookies to include file

--- index.php
+++ index.php
@@ -15,51 +15,10 @@
   <div id="main-content-container">
     <div class="main-content">
 
-      <!-- Cookies -->
-
-      <div>
-        <button id="manage" class="btn btn-cookie" type="button">
-          Manage Consent
-        </button>
-        <div class="cookie-consent">
-          <div class="cookie-box">
-
-            <div class="cookie-title">
-              <h3>Cookies Policy</h3>
-            </div>
-
-            <div class="cookie-content">
-              <p>
-                Our website uses cookies. This helps us provide you with a good experience on our website. To see what
-                cookies
-                we use and what they do, and to opt-in on non-essential cookies click "change settings". For a detailed
-                explanation, click on "<a class="link" href="#">Privacy Policy</a>"
-                otherwise click "Accept cookies" to enter.
-              </p>
-            </div>
-
-            <div class="cookie-btns">
-              <div class="col">
-                <a id="settings" class="btn btn-default btn-block" href="#">Change Settings</a>
-              </div>
-              <div class="col">
-                <a id="accept" class="btn btn-web btn-block" href="#">Accept Cookies</a>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <button class="btn btn-chat" type="button">
-          <span class="icon-chat"></span>
-        </button>
-      </div>
-
-      <!-- Header -->
-
-      <?php @include("php/header.php"); ?>
+      <?php
+      @include("php/cookies.php");
+      @include("php/header.php");
+      ?>
 
       <!-- Hero Banner -->
 
---
+++ php/cookies.php
@@ -0,0 +1,39 @@
+<div>
+        <button id="manage" class="btn btn-cookie" type="button">
+          Manage Consent
+        </button>
+        <div class="cookie-consent">
+          <div class="cookie-box">
+
+            <div class="cookie-title">
+              <h3>Cookies Policy</h3>
+            </div>
+
+            <div class="cookie-content">
+              <p>
+                Our website uses cookies. This helps us provide you with a good experience on our website. To see what
+                cookies
+                we use and what they do, and to opt-in on non-essential cookies click "change settings". For a detailed
+                explanation, click on "<a class="link" href="#">Privacy Policy</a>"
+                otherwise click "Accept cookies" to enter.
+              </p>
+            </div>
+
+            <div class="cookie-btns">
+              <div class="col">
+                <a id="settings" class="btn btn-default btn-block" href="#">Change Settings</a>
+              </div>
+              <div class="col">
+                <a id="accept" class="btn btn-web btn-block" href="#">Accept Cookies</a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <button class="btn btn-chat" type="button">
+          <span class="icon-chat"></span>
+        </button>
+      </div>
