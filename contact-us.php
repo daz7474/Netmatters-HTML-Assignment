9f485fce1f624ca6faeb90f2e8c8738d8aeb56f5
Darren Lindsay <daz7474@gmail.com>
2024-04-11 17:14:15 +0100

    Initial Commit

---
+++ contact-us.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact Us</title>
+  <link rel="stylesheet" type="text/css" href="js/slick/slick.css">
+  <link rel="stylesheet" type="text/css" href="js/slick/slick-theme.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <?php @include("php/header.php") ?>
+
+  <?php @include("php/footer.php") ?>
+  <?php @include("php/side-menu.php") ?>
+  
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <script src="js/slick/slick.min.js"></script>
+  <script src="js/carousel.js"></script>
+  <script src="js/sticky-header.js"></script>
+  <script src="js/side-menu.js"></script>
+  <script src="js/cookies.js"></script>
+</body>
+</html>
