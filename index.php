686a25b617b748010d40d0582a11e0cb747190bf
Darren Lindsay <daz7474@gmail.com>
2024-04-11 23:10:29 +0100

    Moved newsletter to include file

--- index.php
+++ index.php
@@ -761,50 +761,7 @@
 
       <!-- Newsletter -->
 
-      <div class="newsletter">
-        <div class="container">
-
-          <form action="#">
-            <h2>Email Newsletter Sign-Up</h2>
-
-            <div class="sign-up-container">
-              <div class="sign-up-input">
-                <div class="form-group">
-                  <label for="newsletter-name">
-                    <strong>Your Name</strong> <span class="required">*</span>
-                  </label>
-                  <input id="newsletter-name" type="text" name="newsletter-name">
-                </div>
-              </div>
-
-              <div class="sign-up-input">
-                <div class="form-group">
-                  <label for="newsletter-email">
-                    <strong>Your Email</strong> <span class="required">*</span>
-                  </label>
-                  <input id="newsletter-email" type="text" name="newsletter-email">
-                </div>
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label class="policy-container" for="checkbox">
-                <input id="checkbox" type="checkbox">
-                <span>
-                  <strong>Please tick this box if you wish to receive marketing information from us. Please see our <a
-                      href="#">Privacy Policy</a> for more information on how we keep your data safe.</strong>
-                </span>
-              </label>
-            </div>
-
-            <button class="btn" type="submit">
-              Subscribe
-            </button>
-
-          </form>
-
-        </div>
-      </div>
+      <?php @include("php/newsletter.php") ?>
 
       <!-- Footer -->
 
