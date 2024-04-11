8f86c9e2ba3930593e30c1b7c3ab0d6cc648d669
Darren Lindsay <daz7474@gmail.com>
2024-04-11 18:28:33 +0100

    Updated title

--- contact-us.php
+++ contact-us.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Contact Us</title>
+  <title>Contact Us | Netmatters</title>
   <link rel="stylesheet" type="text/css" href="js/slick/slick.css">
   <link rel="stylesheet" type="text/css" href="js/slick/slick-theme.css">
   <link rel="stylesheet" href="css/style.css">
