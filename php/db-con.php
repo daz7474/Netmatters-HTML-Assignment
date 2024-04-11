d30fd92c6f0a7bc4946cac0256073ab4b89855ad
Darren Lindsay <daz7474@gmail.com>
2024-04-11 17:13:54 +0100

    Fixed db location

--- php/db-con.php
+++ php/db-con.php
@@ -1,12 +1,19 @@
 <?php
 
-require_once "../.env";
-
 try {
-  $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-  echo "Connected to $dbname at $host successfully.";
-} catch (PDOException $pe) {
-  die ("Could not connect to the database $dbname :" . $pe->getMessage());
+  $env = parse_ini_file(".env");
+  $host = $env["DB_HOST"];
+  $dbname = $env["DB_NAME"];
+  $username = $env["DB_USERNAME"];
+  $password = $env["DB_PASSWORD"];
+
+  try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+  } catch (PDOException $e) {
+    die ("Could not connect to the database $dbname :" . $e->getMessage());
+  }
+} catch (PDOException $e) {
+  $_SESSION["errMsg"] = "Database info is not correct";
 }
 
 ?>
