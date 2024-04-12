e21b100b5b1f42c22f12be4cd17bb927cfb3cff6
Darren Lindsay <daz7474@gmail.com>
2024-04-12 20:42:49 +0100

    Fixed db path

--- include/news-article.php
+++ include/news-article.php
@@ -1,6 +1,21 @@
 <?php
 
-include("db-con.php");
+$pdo = null;
+
+try {
+    $env = parse_ini_file(".env");
+    $host = $env["DB_HOST"];
+    $dbname = $env["DB_NAME"];
+    $username = $env["DB_USERNAME"];
+    $password = $env["DB_PASSWORD"];
+
+    // Create a new PDO instance
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    error_log("Connection error: " . $e->getMessage());
+}
 
 function getNews($pdo)
 {
