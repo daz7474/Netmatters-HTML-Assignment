ef00299c1ef10774d39179bf577fcd2022a87ef4
Darren Lindsay <daz7474@gmail.com>
2024-04-12 15:57:44 +0100

    Updated error displaying

--- js/contact.js
+++ js/contact.js
@@ -15,13 +15,62 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
     method: "POST",
     body: formData
   })
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw response;
+      }
+      return response.json();
+    })
     .then(data => {
       if (data.errors) {
-        alert("Errors were found: " + data.errors.join(", "));
+        // Clear any messages
+        const successMsg = document.getElementById('success-message');
+        if (successMsg) successMsg.textContent = '';
+
+        // Display error message
+        Object.keys(data.errors).forEach(key => {
+          let errorSpan = document.getElementById('error-' + key);
+          if (errorSpan) {
+            errorSpan.textContent = data.errors[key];
+          }
+        });
       } else {
-        alert(data.success);
+        // Clear any messages
+        document.querySelectorAll('.error-span').forEach(span => span.textContent = '');
+
+        // Display success message
+        let successSpan = document.getElementById('success-message');
+        if (successSpan) {
+          successSpan.textContent = data.success;
+          // Clear the form
+          document.getElementById("contact-form").reset();
+          setTimeout(() => {
+            successSpan.textContent = '';
+          }, 3000);
+        }
       }
     })
-    .catch(error => alert("Failed to process form: " + error));
+    .catch(error => {
+      error.json().then(body => {
+        // Clear any messages
+        const successMsg = document.getElementById('success-message');
+        if (successMsg) successMsg.textContent = '';
+
+        // Handle HTTP response error
+        if (body && body.errors) {
+          Object.keys(body.errors).forEach(key => {
+            let errorSpan = document.getElementById('error-' + key);
+            if (errorSpan) {
+              errorSpan.textContent = body.errors[key];
+            }
+          });
+        } else {
+          // Generic error
+          let errorSpan = document.getElementById('error-general');
+          if (errorSpan) {
+            errorSpan.textContent = 'Failed to process form.';
+          }
+        }
+      });
+    });
 });
