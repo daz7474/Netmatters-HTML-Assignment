1a53859d0381ed90830bc7ab66a05bac6ccadb81
Darren Lindsay <daz7474@gmail.com>
2024-04-12 20:28:17 +0100

    Added missing semi colons

--- contact-us.php
+++ contact-us.php
@@ -345,14 +345,14 @@
         </div>
       </div>
 
-    <?php @include("include/newsletter.php") ?>
+    <?php @include("include/newsletter.php"); ?>
 
-    <?php @include("include/footer.php") ?>
+    <?php @include("include/footer.php"); ?>
 
     </div>
   </div>
 
-  <?php @include("include/side-menu.php") ?>
+  <?php @include("include/side-menu.php"); ?>
   
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"
     integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
