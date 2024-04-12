53c8effdb2e1ddec620a8c4c265083b1ad8b1973
Darren Lindsay <daz7474@gmail.com>
2024-04-12 17:03:35 +0100

    Updated variable name and foreach loop

--- include/news-article.php
+++ include/news-article.php
@@ -1,12 +1,14 @@
 <?php
 
-function getNews($conn)
+include("db-con.php");
+
+function getNews($pdo)
 {
   try {
-    $sql = $conn->query("SELECT * FROM news");
+    $sql = $pdo->query("SELECT * FROM news");
     return $sql->fetchAll();
   } catch (PDOException $e) {
-    echo "Database query failed: " . $e->getMessage();
+    echo "Database query failed: " . htmlspecialchars($e->getMessage());
     exit;
   }
 }
@@ -18,31 +20,28 @@ function newsContent($image, $imageAlt, $title, $readTime, $info, $button, $type
                 <div class="item">
                   <a class="item-link" href="#"></a>
                   <div class="img-container">
-                    <a class="news-tag news-tag-' . $counter . '" href="#">' . $type . '</a>
-                    <img src="' . $image . '" alt="">
+                    <a class="news-tag news-tag-' . htmlspecialchars($counter) . '" href="#">' . htmlspecialchars($type) . '</a>
+                    <img src="' . htmlspecialchars($image) . '" alt="' . htmlspecialchars($imageAlt) . '">
                   </div>
 
                   <div class="content">
-                    <h3 class="heading-' . $counter . '">
-                      ' . $title . '
-                      <span class="read-time">' . $readTime . '</span>
+                    <h3 class="heading-' . htmlspecialchars($counter) . '">
+                      ' . htmlspecialchars($title) . '
+                      <span class="read-time">' . htmlspecialchars($readTime) . '</span>
                     </h3>
 
-                    <p>
-                      ' . $info . '
-                    </p>
-
-                    <a class="btn btn-' . $button . '" href="#">Read More</a>
+                    <p>' . htmlspecialchars($info) . '</p>
+                    <a class="btn btn-' . htmlspecialchars($button) . '" href="#">Read More</a>
 
                     <div class="user">
                       <div class="avatar">
-                        <img src="' . $authorImage . '" alt="">
+                        <img src="' . htmlspecialchars($authorImage) . '" alt="">
                       </div>
 
                       <div class="details">
-                        <strong>Posted by ' . $authorName . '</strong>
+                        <strong>Posted by ' . htmlspecialchars($authorName) . '</strong>
                         <br>
-                        ' . $date . '
+                        ' . htmlspecialchars($date) . '
                       </div>
                     </div>
                   </div>
@@ -51,21 +50,24 @@ function newsContent($image, $imageAlt, $title, $readTime, $info, $button, $type
   ';
 }
 
-include "db-con.php";
-
-foreach(getNews($conn) as $news) {
-  echo newsContent(
-    $news["image"],
-    $news["image_alt"], 
-    $news["title"], 
-    $news["read_time"], 
-    $news["info"],
-    $news["button"],
-    $news['type'], 
-    $news["author_image"], 
-    $news["author_name"], 
-    $news["date"], 
-    $news["counter"]);
+if ($pdo instanceof PDO) {
+  foreach (getNews($pdo) as $news) {
+      echo newsContent(
+          $news["image"],
+          $news["image_alt"],
+          $news["title"],
+          $news["read_time"],
+          $news["info"],
+          $news["button"],
+          $news['type'],
+          $news["author_image"],
+          $news["author_name"],
+          $news["date"],
+          $news["counter"]
+      );
+  }
+} else {
+  echo "Failed to get news items.";
 }
 
 ?>
