5b6449a098b0de27605c4c837b4ea56d01074317
Darren Lindsay <daz7474@gmail.com>
2024-04-12 20:26:10 +0100

    Added missing semi colons

--- index.php
+++ index.php
@@ -761,18 +761,18 @@
 
       <!-- Newsletter -->
 
-      <?php @include("include/newsletter.php") ?>
+      <?php @include("include/newsletter.php"); ?>
 
       <!-- Footer -->
 
-      <?php @include("include/footer.php") ?>
+      <?php @include("include/footer.php"); ?>
 
     </div>
   </div>
 
   <!-- Side Menu -->
 
-  <?php @include("include/side-menu.php") ?>
+  <?php @include("include/side-menu.php"); ?>
 
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"
     integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
