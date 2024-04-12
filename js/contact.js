84b06fb31f53da8075ad46c7e4d3f5684ddeed25
Darren Lindsay <daz7474@gmail.com>
2024-04-12 15:33:24 +0100

    Form response handling

--- js/contact.js
+++ js/contact.js
@@ -5,3 +5,23 @@ document.querySelector(".accordion").addEventListener("click", () => {
   // Add active to accordion
   accordionInfo.classList.toggle("accordion-active");
 });
+
+// Form response
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let formData = new FormData(this);
+
+  fetch(this.action, {
+    method: "POST",
+    body: formData
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.errors) {
+        alert("Errors were found: " + data.errors.join(", "));
+      } else {
+        alert(data.success);
+      }
+    })
+    .catch(error => alert("Failed to process form: " + error));
+});
