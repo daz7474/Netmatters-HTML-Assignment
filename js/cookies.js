a0dfca6f1af2f15ac85e5b652fa58b85faaf2537
Darren Lindsay <daz7474@gmail.com>
2024-04-13 21:09:45 +0100

    Updated

--- js/cookies.js
+++ js/cookies.js
@@ -1,5 +1,5 @@
 const consentBtn = document.getElementById("manage");
-const cookiePopup = document.getElementsByClassName("cookie-consent");
+const cookiePopup = document.querySelector(".cookie-consent");
 const acceptBtn = document.getElementById("accept");
 const main = document.querySelector("body");
 
@@ -7,20 +7,20 @@ const main = document.querySelector("body");
 window.onload = () => {
   // If not stored in local storage, show popup
   if (localStorage.getItem("policyAccepted") === false || localStorage.getItem("policyAccepted") === null) {
-    window.onload = cookiePopup[0].classList.toggle("cookie-show");
+    window.onload = cookiePopup.classList.toggle("cookie-show");
     main.classList.add("lock");
   }
 };
 
 consentBtn.addEventListener("click", () => {
   // Open popup on manage consent click
-  cookiePopup[0].classList.toggle("cookie-show");
+  cookiePopup.classList.toggle("cookie-show");
   main.classList.add("lock");
 });
 
 acceptBtn.addEventListener("click", (e) => {
   // Close popup on accept button click
-  cookiePopup[0].classList.toggle("cookie-show");
+  cookiePopup.classList.toggle("cookie-show");
   main.classList.remove("lock");
   // Save state to local storage
   localStorage.setItem("policyAccepted", true);
