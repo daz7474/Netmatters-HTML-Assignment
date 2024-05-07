d03044e739c554f93601137b70cf4e377f85e1b4
Darren Lindsay <daz7474@gmail.com>
2024-05-07 19:05:39 +0100

    Replaced db connection with requrie_once

--- include/news-article.php
+++ include/news-article.php
@@ -1,27 +1,12 @@
 <?php
 
-$pdo = null;
-
-try {
-    $env = parse_ini_file(".env");
-    $host = $env["DB_HOST"];
-    $dbname = $env["DB_NAME"];
-    $username = $env["DB_USERNAME"];
-    $password = $env["DB_PASSWORD"];
-
-    // Create a new PDO instance
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    error_log("Connection error: " . $e->getMessage());
-}
+require_once("db-con.php");
 
 function getNews($pdo)
 {
   try {
     // Queries everything from news table
-    $sql = $pdo->query("SELECT * FROM news");
+    $sql = $pdo->query("SELECT * FROM news ORDER BY date DESC LIMIT 3");
     return $sql->fetchAll();
   } catch (PDOException $e) {
     echo "Database query failed: " . htmlspecialchars($e->getMessage());
